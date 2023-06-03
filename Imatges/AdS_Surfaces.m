0fdfc796df7894e09aa7e12a5edf3a9b39e7e4e3
FerranRMascaro <ferran.r.m11@gmail.com>
2023-06-03 20:58:57 +0200

    202306032058

--- Imatges/AdS_Surfaces.m
+++ Imatges/AdS_Surfaces.m
@@ -1,5 +1,43 @@
-pgon1 = polyshape([0 20 50 30],[50 60 60 50])
-pgon2 = polyshape([5 22 45 28],[38 45 45 38])
-plot(pgon1)
-plot(pgon2)
-axis off
+% Parámetros de la imagen
+ancho = 500;          % Ancho de la imagen
+alto = 500;           % Alto de la imagen
+numPlanos = 10;       % Número de planos
+colorFondo = [255 255 255];  % Color de fondo (blanco)
+
+% Crear una matriz vacía para la imagen
+imagen = uint8(zeros(alto, ancho, 3));
+
+% Calcular la diferencia de tamaño entre los planos
+diferenciaTamano = ceil(alto / numPlanos);
+
+% Generar los planos paralelos
+for i = 1:numPlanos
+    % Calcular el tamaño del plano actual
+    tamanoPlano = alto - (i-1) * diferenciaTamano;
+    
+    % Calcular las coordenadas verticales del plano
+    inicio = (alto - tamanoPlano) + 1;
+    fin = alto - (i-1) * diferenciaTamano;
+    
+    % Asignar un color al plano actual
+    colorPlano = randi([0 255], 1, 3);
+    
+    % Crear una matriz auxiliar con el color del plano repetido
+    plano = repmat(colorPlano, tamanoPlano, ancho);
+    
+    % Asignar el color del plano a la imagen
+    imagen(inicio:fin, :, 1) = plano(:, :, 1);
+    imagen(inicio:fin, :, 2) = plano(:, :, 2);
+    imagen(inicio:fin, :, 3) = plano(:, :, 3);
+end
+
+% Configurar el color de fondo
+imagen(imagen == 0) = colorFondo(1);
+
+% Mostrar la imagen
+figure;
+imshow(imagen);
+title('Planos paralelos ordenados verticalmente');
+
+% Guardar la imagen en un archivo
+imwrite(imagen, 'planos_paralelos.png');  % Cambia el nombre y formato del archivo según tus necesidades
